2dbdfa7a2a30011a5d779c68f778a069c9b54573
jdohun <wjdehgns97@naver.com>
2023-12-01 01:25:06 +0900

    feat: 명확한 메소드명으로 변경 및 에러메시지 상수화
    InputHandler.java
    InputHandlerErrorMessage.java

---
+++ src/main/java/lotto/handler/InputHandlerErrorMessage.java
@@ -0,0 +1,20 @@
+package lotto.handler;
+
+import static lotto.handler.InputHandler.LOTTO_PRICE;
+import static lotto.util.ErrorMessagePrefix.ERROR_MESSAGE_PREFIX;
+
+enum InputHandlerErrorMessage {
+    ERROR_NOT_DIVISIBLE_BY_LOTTO_PRICE(LOTTO_PRICE + " 단위의 금액만 입력 가능합니다."),
+    ERROR_NOT_INTEGER("정수가 아닙니다.")
+    ;
+
+    private final String message;
+
+    InputHandlerErrorMessage(String message) {
+        this.message = ERROR_MESSAGE_PREFIX + message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
