d83f0245d178002d1f884582060ea3ae74c09ce0
jdohun <wjdehgns97@naver.com>
2023-11-30 21:49:38 +0900

    feat: 에러메시지 접두사를 생성자에서 추가하도록 수정
    StringValidatorErrorMessage.java

--- src/main/java/lotto/util/stringValidator/StringValidatorErrorMessage.java
+++ src/main/java/lotto/util/stringValidator/StringValidatorErrorMessage.java
@@ -3,13 +3,13 @@ package lotto.util.stringValidator;
 import static lotto.util.ErrorMessagePrefix.ERROR_MESSAGE_PREFIX;
 
 enum StringValidatorErrorMessage {
-    ERROR_NULL(ERROR_MESSAGE_PREFIX + "null 을 입력할 수 없습니다."),
-    ERROR_EMPTY(ERROR_MESSAGE_PREFIX + "빈 문자열을 입력할 수 없습니다."),
-    ERROR_HAS_SURROUNDING_WHITE_SPACE(ERROR_MESSAGE_PREFIX + "문자열의 시작과 끝에 공백을 입력할 수 없습니다.");
+    ERROR_NULL("null 을 입력할 수 없습니다."),
+    ERROR_EMPTY("빈 문자열을 입력할 수 없습니다."),
+    ERROR_HAS_SURROUNDING_WHITE_SPACE("문자열의 시작과 끝에 공백을 입력할 수 없습니다.");
     private final String message;
 
     StringValidatorErrorMessage(String message) {
-        this.message = message;
+        this.message = ERROR_MESSAGE_PREFIX + message;
     }
 
     public String getMessage() {
