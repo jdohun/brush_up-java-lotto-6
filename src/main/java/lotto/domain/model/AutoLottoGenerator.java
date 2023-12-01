039e5b335c2579ae52c4f5e062f9d1ee42d1316e
jdohun <wjdehgns97@naver.com>
2023-12-01 18:57:31 +0900

    refactor: 로또 생성기 이름 변경
    LottoGenerator.java -> AutoLottoGenerator.java

--- src/main/java/lotto/domain/model/AutoLottoGenerator.java
+++ src/main/java/lotto/domain/model/AutoLottoGenerator.java
@@ -1,15 +1,17 @@
 package lotto.domain.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.model.lotto.Lotto;
+import lotto.domain.model.lottoNumber.LottoNumber;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public final class LottoGenerator {
-    private LottoGenerator() {
+public final class AutoLottoGenerator {
+    private AutoLottoGenerator() {
     }
 
-    public static LottoGenerator getInstance() {
+    public static AutoLottoGenerator getInstance() {
         return Holder.LOTTO_GENERATOR;
     }
 
@@ -28,6 +30,6 @@ public final class LottoGenerator {
     }
 
     private static final class Holder {
-        private static final LottoGenerator LOTTO_GENERATOR = new LottoGenerator();
+        private static final AutoLottoGenerator LOTTO_GENERATOR = new AutoLottoGenerator();
     }
 }
