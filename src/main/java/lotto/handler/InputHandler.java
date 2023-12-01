ed81ab53c36d79038e67e881c00edafbf153bad9
DH Jeong <wjdehgns97@gmail.com>
2023-12-01 22:32:20 +0900

    feat: 입력 받은 구입 금액이 1000 으로 나누어 떨어지는지 InputHandler 에서 검증한다.
    InputHandler.java
    InputHandlerErrorMessage.java
    AutoLottoGenerator.java
    AutoLottoGeneratorErrorMessage.java 제거

--- src/main/java/lotto/handler/InputHandler.java
+++ src/main/java/lotto/handler/InputHandler.java
@@ -1,5 +1,6 @@
 package lotto.handler;
 
+import lotto.domain.model.AutoLottoGenerator.AutoLottoGenerator;
 import lotto.util.stringValidator.StringValidator;
 
 import java.util.regex.Matcher;
@@ -15,16 +16,24 @@ public final class InputHandler {
         return Holder.INPUT_HANDLER;
     }
 
-    public int parseInputMoneyAsInteger(String inputMoney) {
+    public int parseInputMoneyAsInteger(final String inputMoney) {
         StringValidator.isNotNull(inputMoney);
         StringValidator.isNotEmpty(inputMoney);
         StringValidator.hasNotSurroundingWhiteSpace(inputMoney);
-        return parseInputAsInteger(inputMoney);
+        final int money = parseInputAsInteger(inputMoney);
+        validateMoneyDivisibleByLottoPrice(money);
+        return money;
+    }
+
+    private void validateMoneyDivisibleByLottoPrice(final int money) {
+        if (money % AutoLottoGenerator.LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(InputHandlerErrorMessage.ERROR_NOT_DIVISIBLE_BY_LOTTO_PRICE.getMessage());
+        }
     }
 
     private int parseInputAsInteger(String input) {
         Matcher matcher = INTEGER_PATTERN.matcher(input);
-        if (!matcher.matches()){
+        if (!matcher.matches()) {
             throw new IllegalArgumentException(InputHandlerErrorMessage.ERROR_NOT_INTEGER.getMessage());
         }
         return Integer.parseInt(input);
