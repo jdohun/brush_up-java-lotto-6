0e1f525773d6ea4b5a56f0918239db85ba713fa2
jdohun <wjdehgns97@naver.com>
2023-12-02 18:50:17 +0900

    feat: Lotteries 내부 정보를 dto List 로 반환하는 기능, 저장된 Lotto 수량을 반환하는 기능 추가
    Lotteries.java

--- src/main/java/lotto/domain/model/Lotteries.java
+++ src/main/java/lotto/domain/model/Lotteries.java
@@ -1,7 +1,9 @@
 package lotto.domain.model;
 
 import lotto.domain.model.lotto.Lotto;
+import lotto.dto.LottoDto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotteries {
@@ -14,5 +16,17 @@ public class Lotteries {
 
     public static Lotteries from(List<Lotto> lottoList) {
         return new Lotteries(lottoList);
+    }
+
+    public int getSize() {
+        return lotteries.size();
+    }
+
+    public List<LottoDto> toDtos() {
+        List<LottoDto> lottoDtos = new ArrayList<>();
+        for (Lotto lotto : lotteries) {
+            lottoDtos.add(lotto.mapToDto());
+        }
+        return lottoDtos;
     }
 }
