8f9d4037164d3b0dd2e947365f3f05e88a8d3009
jdohun <wjdehgns97@naver.com>
2023-12-04 18:52:55 +0900

    fix: 당첨 통계 출력 시 개수 부분을 분리하여 초기화 시 format 처리 에러, 당첨 통계 구분줄이 상수명으로 출력되는 버그 해결
    WinningResult.java
    OutputMessage.java
    OutputView.java

--- src/main/java/lotto/view/outputView/OutputMessage.java
+++ src/main/java/lotto/view/outputView/OutputMessage.java
@@ -3,7 +3,8 @@ package lotto.view.outputView;
 enum OutputMessage {
     NUMBER_OF_BOUGHT_LOTTERIES_FORMAT("%d개를 구매했습니다."),
     WINNING_STATISTICS_TITLE("당첨 통계"),
-    LINE_SEPARATOR_AS_HYPHEN("---"),
+    LINE_SEPARATOR_FOR_STATISTICS("---"),
+    COUNT_OF_WINNING_FORMAT(" - %d개"),
     RESULT_RATE_OF_RETURN_FORMAT("총 수익률은 %.1f%%입니다.");
 
     private final String message;
