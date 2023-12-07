fee1e993f7c8bbeecb11b26406441c19a3801aed
jdohun <wjdehgns97@naver.com>
2023-12-07 19:21:22 +0900

    feat: Lotteries 추가 및 수정된 기능에 따라 메시지 추가
    LotteriesErrorMessage.java

--- src/main/java/lotto/domain/model/lotteries/LotteriesErrorMessage.java
+++ src/main/java/lotto/domain/model/lotteries/LotteriesErrorMessage.java
@@ -3,7 +3,8 @@ package lotto.domain.model.lotteries;
 import static lotto.util.ErrorMessagePrefix.ERROR_MESSAGE_PREFIX;
 
 public enum LotteriesErrorMessage {
-    ERROR_EMPTY_LIST("전달받은 값이 비어있습니다."),
+    ERROR_NULL("null 을 전달받을 수 없습니다."),
+    ERROR_EMPTY_LIST("전달받은 리스트가 비어있습니다."),
     ERROR_CONTAIN_NULL_ELEMENT("전달받은 값에 null 이 포함되어 있습니다.");
 
     private final String message;
